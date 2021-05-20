9508ca25ab116347cdcf5afd98bd1fc1b00f69e5
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-05-20 01:06:35 -0600

    add ten_actors facade, review facade still under construction

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -3,4 +3,19 @@ class MovieFacade
     movie_info = FilmService.get_movie_info(movie_id)
     Movie.new(movie_info)
   end
+
+  def self.ten_actors(movie_id)
+    actors = FilmService.get_first_10_credits(movie_id)
+    actors.map {|actor| Actor.new(actor)}
+  end
+
+  def self.reviews(movie_id)
+    # binding.pry
+    reviews = FilmService.get_movie_reviews(movie_id)
+    reviews.map {|review| Review.new(review)}
+  end
+
+
+
+
 end
