d9edddef0b5d3e61c2ff5e6a93a926aa39f850e7
loganjacob76 <nagolisback@gmail.com>
2021-05-20 00:50:48 -0600

    Add helper method

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Guest do
     it { should belong_to :user }
     it { should belong_to :party }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @user = create(:user)
+      @guest = create(:guest, user: @user)
+    end
+    
+    it '#name' do
+      expect(@guest.name).to eq(@user.name)
+    end
+  end
 end
