2affa973ec5a351ea1420e5de5ba88cffad4fcd0
loganjacob76 <nagolisback@gmail.com>
2021-05-19 20:54:42 -0600

    Update party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,11 @@
 class Party < ApplicationRecord
   belongs_to :host, class_name: 'User'
+  has_many :guests, dependent: :destroy
+
+  validates :host_id, presence: true, numericality: true
+  validates :movie_id, presence: true, numericality: true
+  validates :duration, presence: true, numericality: true
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :title, presence: true
 end
